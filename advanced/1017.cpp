fd72462ba5a95a61c86c7ac35fd4cb3cb59f8e28
PatrickHao <943526723@qq.com>
2021-02-22 23:29:22 +0800

    20210222

--- advanced/1017.cpp
+++ advanced/1017.cpp
@@ -1,72 +1,72 @@
-#include <cstdio>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-const int ot = 8 * 60 * 60;
-const int ct = 17 * 60 * 60;
 
 struct elem {
-    int at;
-    int t;
-} D[10010];
+    int s, t;
 
-bool cmp(elem e1, elem e2) {
-    return e1.at < e2.at;
-}
+    elem() {}
+    elem(int s, int t) : s(s), t(t) {}
+
+    friend bool operator < (elem a, elem b) {
+        return a.s < b.s;
+    }
+};
 
 int n, k;
 
+int change(string t) {
+    int h, m, s;
+    h = stoi(t.substr(0, 2));
+    m = stoi(t.substr(3, 2));
+    s = stoi(t.substr(6, 2));
+    return h * 60 * 60 + m * 60 + s;
+}
+
+int getIndex(const vector<int>& a) {
+    for (int i = 0; i < a.size(); i++) if (a[i] == 0) return i;
+    return -1;
+}
+
+bool judge(const vector<int>& a) {
+    for (int i = 0; i < a.size(); i++) if (a[i] != 0) return false;
+    return true;
+}
+
 int main() {
-    freopen("./sample_in/1017.txt", "r", stdin);
-    scanf("%d %d", &n, &k);
-    int index = 0;
-    while (n--) {
-        int h, m, s, tl;
-        scanf("%d:%d:%d %d", &h, &m, &s, &tl);
-        if (h * 60 * 60 + m * 60 + s > ct) {
-            continue;
-        }
-        D[index].at = h * 60 * 60 + m * 60 + s;
-        D[index].t = tl <= 60 ? tl : 60;
-        index++;
+    cin >> n >> k;
+    int start = change("08:00:00"), end = change("17:00:00");
+    vector<elem> data;
+    for (int i = 0; i < n; i++) {
+        string s;
+        elem e;
+        cin >> s >> e.t;
+        e.s = change(s);
+        e.t *= 60;
+        data.push_back(e);
     }
-    sort(D, D + index, cmp);
-
-    int wt = 0;
-    for (int i = 0; i < index; i++) {
-        if (D[i].at < ot) {
-            wt = wt + ot - D[i].at;
-        }
+    sort(data.begin(), data.end());
+    int num = 0;
+    for (auto v : data) {
+        if (v.s > end) break;
+        num++;
     }
 
-    int wi[110] = {0}, cur = 0;
-    for (int i = ot; i <= ct; i++) {
-        bool flag = false;
-        for (int j = 0; j < k; j++) {
-            if (wi[j] == 0 && D[cur].at <= i) {
-                wi[j] = D[cur].t * 60;
-                cur++;
-                if (cur == index) {
-                    flag = true;
-                    break;
-                }
-            }
-        } 
-        for (int j = 0; j < k; j++) {
-            if (wi[j] > 0) {
-                wi[j]--;
+    int curTime = 0, ind = 0, allTime = 0;
+    vector<int> a(k, 0);
+    while (true) {
+        if (ind >= num && judge(a)) break;
+        curTime++;
+        if (curTime >= start) {
+            while (getIndex(a) != -1 && ind < num && curTime >= data[ind].s) {
+                a[getIndex(a)] = data[ind].t;
+                allTime += (curTime - data[ind].s);
+                // printf("%d, %d, %d\n", curTime, data[ind].s, data[ind].t);
+                ++ind;
             }
         }
-        if (flag) {
-            break;
-        }
-        for (int j = cur; j < index; j++) {
-            if (D[j].at <= i) {
-                wt++;
-            }
-        }
+        for (int &v : a) if (v != 0) v--;
     }
-    double wtm = wt / 60 + ((wt % 60) / 60.0);
-    printf("%.1f", wtm / index);
+    printf("%.1f\n", allTime * 1.0 / 60 / num);
+    
     return 0;
 }
